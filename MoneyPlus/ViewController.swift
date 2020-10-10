b00a176bafe5e34035312ec01bf2413c2427d255
FakeCoder <icedotaku.dev@gmail.com>
2020-10-10 20:26:35 +0800

    create repo

---
+++ MoneyPlus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyPlus
+//
+//  Created by FakeCoder on 2020/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
