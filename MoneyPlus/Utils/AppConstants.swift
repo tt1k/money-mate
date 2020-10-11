c1f5dcc368aa0b05ef33001b4908c06b5eef99c1
FakeCoder <icedotaku.dev@gmail.com>
2020-10-11 12:13:30 +0800

    adjust color

--- MoneyPlus/Utils/AppConstants.swift
+++ MoneyPlus/Utils/AppConstants.swift
@@ -9,10 +9,10 @@ import Foundation
 import UIKit
 
 struct AppConstants {
-    static let kPinkColor = UIColor.init(red: 0xFB/255, green: 0x72/255, blue: 0x99/255, alpha: 1)
+    static let kPinkColor = UIColor.init(red: 241/255, green: 172/255, blue: 69/255, alpha: 1)
     static let kWhiteColor = UIColor.white
-    static let kBlueColor = UIColor.blue
-    static let kPurpleColor = UIColor.purple
+    static let kBlueColor = UIColor.init(red: 74/255, green: 88/255, blue: 235/255, alpha: 1)
+    static let kPurpleColor = UIColor.init(red: 195/255, green: 209/255, blue: 251/255, alpha: 1)
     static let kBlackColor = UIColor.black
     static let kLightGrayColor = UIColor.lightGray
     static let kScreenWidth = UIScreen.main.bounds.size.width
